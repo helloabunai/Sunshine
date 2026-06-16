231ec14d5b7ece7f6b7c7a4c5f6442c7fdb7e1a9
helloabunai <alastairm@gmail.com>
2026-06-16 18:36:29 +0300

    select capture monitor with stable string based id across backends; integer fallback

--- src_assets/common/assets/web/configs/tabs/audiovideo/DisplayOutputSelector.vue
+++ src_assets/common/assets/web/configs/tabs/audiovideo/DisplayOutputSelector.vue
@@ -9,7 +9,7 @@ const props = defineProps([
 ])
 
 const config = ref(props.config)
-const outputNamePlaceholder = (props.platform === 'windows') ? '{de9bb7e2-186e-505b-9e93-f48793333810}' : '0'
+const outputNamePlaceholder = (props.platform === 'windows') ? '{de9bb7e2-186e-505b-9e93-f48793333810}' : 'HDMI-A-1'
 </script>
 
 <template>
